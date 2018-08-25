1e60314bf79c3986d163e091e0f012abc30703b9
Jason McDermott <jmcdermott@rightpoint.com>
2018-08-25 10:29:25 -0500

    refactor to use indexer syntax

--- NumeralSystemConversion.Converters/DecimalToHex.cs
+++ NumeralSystemConversion.Converters/DecimalToHex.cs
@@ -6,23 +6,23 @@ namespace NumeralSystemConversion.Converters
     public class DecimalToHex
     {
         private const double Base = 16.0;
-        private Dictionary<int, string> _codes = new Dictionary<int, string>
+        private readonly Dictionary<int, string> _codes = new Dictionary<int, string>
         {
-            { 1, "1" },
-            { 2, "2" },
-            { 3, "3" },
-            { 4, "4" },
-            { 5, "5" },
-            { 6, "6" },
-            { 7, "7" },
-            { 8, "8" },
-            { 9, "9" },
-            { 10, "A" },
-            { 11, "B" },
-            { 12, "C" },
-            { 13, "D" },
-            { 14, "E" },
-            { 15, "F" }
+            [1] = "1",
+            [2] = "2",
+            [3] = "3",
+            [4] = "4",
+            [5] = "5",
+            [6] = "6",
+            [7] = "7",
+            [8] = "8",
+            [9] = "9",
+            [10] = "A",
+            [11] = "B",
+            [12] = "C",
+            [13] = "D",
+            [14] = "E",
+            [15] = "F"
         };
         
         public string Convert(int input)
